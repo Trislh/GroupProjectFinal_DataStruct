8da7333d435bc53f3e8415ac40727ed317d9ff79
Triet-luu <56990982+Triet-luu@users.noreply.github.com>
2022-01-07 14:11:51 +0700

    Add files via upload

--- GroupProjectFinal/src/Algorithm.java
+++ GroupProjectFinal/src/Algorithm.java
@@ -4,7 +4,7 @@ public class Algorithm {
     private char[][]  mine;
     private int minSteps;
     private int maxGolds;
-    ArrayList<Character> Path = new ArrayList<>();
+    ArrayList<Character> Optimal = new ArrayList<>();
 
 
     public Algorithm(char[][] mine) {
@@ -12,7 +12,28 @@ public class Algorithm {
     }
 
     public void exhaustiveSearch() {
-
+        for (int i = 0; i <= mine.length; i++)
+        {
+            for (int j = 0; j <= (Math.pow(2.0,mine.length)-1); j++)
+            {
+                ArrayList<Character> Path = new ArrayList<>();
+                for (int k = 0; k <= i - 1; k++)
+                {
+                    j = (j >> k) & 1;
+                    if (j == 1)
+                    {
+                        if (mine[i][j+1] != 'X')
+                        {
+                            i++;
+                        }
+                        if (mine[i+1][j] != 'X')
+                        {
+                            j++;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     public void dynamicProgrammingSearch() {
